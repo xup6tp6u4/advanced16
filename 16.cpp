c958740bc6c72ebfc603b1f9cb62527e54739e2a
xup6tp6u4 <48340287+xup6tp6u4@users.noreply.github.com>
2019-05-30 17:47:56 +0800

    Add files via upload

---
+++ 16.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int add(void);
+
+int main(void){
+    printf("2+4+6+~+100¥[Á`:%d\n", add());  
+    system("pause");
+    return 0;
+}
+int add(void){
+    static int sum=0;
+    int i;
+    for(i=2;i<=100;i+=2){
+        sum+=i;
+    }
+    return sum;
+}
